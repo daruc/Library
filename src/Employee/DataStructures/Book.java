a03d89b305d58b894cb1c98691fb3090aff712bf
darek40bc <darek40bc@vp.pl>
2015-12-31 17:38:56 +0100

    I added Book data structure.

---
+++ src/Employee/DataStructures/Book.java
@@ -0,0 +1,13 @@
+/**
+ * Created by darek on 29.12.2015.
+ */
+package Employee.DataStructures;
+public class Book {
+    public String title = "";
+    public String description = "";
+    public String author = "";
+    public String isbn = "";
+    public String genre = "";
+    public int number_of_books = 0;
+    public int borrowed = 0;
+}
